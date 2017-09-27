b1f782344077301830ef3b28639a730aed69529a
Qwertas2166 <eugene.suvorikov@gmail.com>
2017-09-27 23:41:44 +0300

    1st exercise (logger 1)

--- src/main/java/com/acme/edu/Logger.java
+++ src/main/java/com/acme/edu/Logger.java
@@ -1,11 +1,43 @@
 package com.acme.edu;
 
 public class Logger {
-    public static void log(int message) {
-        System.out.println("primitive: " + message);
-    }
+
+
+    //region const
+    private static final String PRIMITIVE_PREFIX = "primitive: ";
+    private static final String CHAR_PREFIX = "char: ";
+    private static final String REF_PREFIX = "reference: ";
+    private static final String STRING_PREF = "string: ";
+
+    //endregion
 
     public static void log(byte message) {
-        System.out.println("primitive: " + message);
+        print(PRIMITIVE_PREFIX + "\n" + message);
     }
+
+    public static void log(int message) {
+        print(PRIMITIVE_PREFIX + message + "\n");
+    }
+
+    public static void log(boolean message) {
+        print(PRIMITIVE_PREFIX + message);
+    }
+
+    public static void log(char message) {
+        print(CHAR_PREFIX +"\n" + message);
+    }
+
+    public static void log(String message) {
+        print(STRING_PREF + message);
+    }
+
+    public static void log(Object message) {
+        print(REF_PREFIX + "@"+ message);
+    }
+
+
+    private static void print(String message) {
+        System.out.print(message);
+    }
+
 }
